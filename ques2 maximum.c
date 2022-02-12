ddbef78480ddbd6873ae7dfd5c91f14ded7a33a4
AnanyaM007 <99496164+AnanyaM007@users.noreply.github.com>
2022-02-12 17:24:21 +0530

    Comparision of two numbers

--- ques2 maximum.c
+++ ques2 maximum.c
@@ -9,11 +9,15 @@ int main()
     {
         printf("%d is maximum",num1);
     }
-    if(num2>num1);
+    else if (num1==num2)
     {
-        printf("%d is maximum", num2);
+        printf("both are equal");
     }
-    
+    else
+    {
+        printf("%d is maximum",num2);
+    }
     return 0;
 }
 
+    
